c94f7fd370b023012cb82eb6e0ef1e18f150bfea
Valeria Nahuelpán <valeria.nahuelpan@gmail.com>
2025-11-17 01:55:14 -0300

    Add profile route with JWT validation

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -98,3 +98,21 @@ export const logout = (req, res) => {
     res.cookie('token', '', {expires: new Date(0)});
     return res.sendStatus(200)
 }
+
+export const profile = async (req, res) => {
+    //console.log(req.user)
+    const userFound = await User.findById(req.user.id);
+
+    if(!userFound)
+        return res.status(400).json({message: "User not found"});
+
+    return res.json({
+        id: userFound._id, //api responde con solo el id, nombre y email del usuario creado
+        email: userFound.email,
+        username: userFound.username,
+        createdAt: userFound.createdAt,
+        updatedAt: userFound.updatedAt,
+    });
+
+    res.send('profile')
+}
---
+++ src/middlewares/validateToken.js
@@ -0,0 +1,15 @@
+import jwt from 'jsonwebtoken'
+import { TOKEN_SECRET } from '../config.js';
+export const authRequired = (req, res, next) => {
+    //console.log(req.headers);
+    const {token} = req.cookies; // Se puede usar gracias al paquete cookie-parser
+    console.log(token)
+    if (!token)
+        return res.status(401).json({message:"No token, authorization denied"});
+    jwt.verify(token, TOKEN_SECRET, (err, user)=> {
+        if (err) return res.status(403).json({message: "Invalid token"});
+        req.user = user
+        next()    
+    })
+    
+}
