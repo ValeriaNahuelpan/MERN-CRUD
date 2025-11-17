299d0877a7e78ec4e44b7c57d155513a0db4d36d
Valeria Nahuelpán <valeria.nahuelpan@gmail.com>
2025-11-17 19:07:13 -0300

    Implement full tasks CRUD (model, controller and routes)

--- src/app.js
+++ src/app.js
@@ -1,10 +1,12 @@
 import express from 'express'; // para usar import en vez de require, poner "type":"module" en package.json
 import morgan from 'morgan'; // HTTP request logger
 import authRoutes from './routes/auth.routes.js';
+import tasksRoutes from './routes/tasks.routes.js'
 import cookieParser from 'cookie-parser';
 const app = express();
 app.use(morgan('dev')); // run dev
 app.use(express.json())
 app.use(cookieParser())
 app.use('/api', authRoutes); // todas las rutas empiezan con api
+app.use('/api', tasksRoutes);
 export default app;
